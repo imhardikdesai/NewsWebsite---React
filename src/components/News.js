bb3ae497299a5d30d4dec9beaf9bd150c8449f65
Hardik Desai <hp676913@gmail.com>
2022-08-17 11:41:19 +0530

    Navigations of Page Numbers with Buttons

--- src/components/News.js
+++ src/components/News.js
@@ -6,16 +6,46 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
+
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=1&pageSize=9`;
         let data = await fetch(url);
         let newData = await data.json();
         this.setState({
-            articles: newData.articles
+            articles: newData.articles,
+            totalResults: newData.totalResults
         })
+    }
+
+    btnPrev = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page - 1}&pageSize=9`;
+        let data = await fetch(url);
+        let newData = await data.json();
+        this.setState({
+            articles: newData.articles,
+            page: this.state.page - 1
+        })
+        document.getElementById('btnNext').removeAttribute("disabled", "")
+    }
+
+    btnNext = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 9)) {
+            document.getElementById('btnNext').setAttribute('disabled', "")
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page + 1}&pageSize=9`;
+            let data = await fetch(url);
+            let newData = await data.json(this.state.articles);
+            this.setState({
+                articles: newData.articles,
+                totalResults: newData.totalResults,
+                page: this.state.page + 1
+            })
+        }
     }
     render() {
         return (
@@ -27,6 +57,10 @@ export class News extends Component {
                             <NewsItem title={element.title ? element.title : "Empty Title"} imageUrl={element.urlToImage ? element.urlToImage : "https://asianatimes.com/wp-content/uploads/2022/08/asteroid-6054186_1920_1659586399339_1659712789934_1659712789934.jpg"} description={element.description ? element.description : "Empty Description"} url={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.btnPrev}>&larr; Privious</button>
+                    <button id="btnNext" className="btn btn-dark" onClick={this.btnNext}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a target="_blank" rel="noreferrer" href={url} className="btn btn-sm btn-primary">Read more</a>
+                        <a target="_blank" rel="noreferrer" href={url} className="btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
             </div>
