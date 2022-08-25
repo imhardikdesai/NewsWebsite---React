10dbb68eddcd56399ed1e18b955c9d588b377943
Hardik Desai <hp676913@gmail.com>
2022-08-25 11:09:07 +0530

    add router dom to fetch differnt news category

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    static defaultProps = {
+        country: 'in',
+        category: 'general',
+        pageSize: 9
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        category: PropTypes.string,
+        pageSize: PropTypes.number
+    }
     constructor() {
         super();
         this.state = {
@@ -16,7 +28,7 @@ export class News extends Component {
         this.setState({
             loading: true
         })
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let newData = await data.json();
         this.setState({
@@ -30,7 +42,7 @@ export class News extends Component {
         this.setState({
             loading: true
         })
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let newData = await data.json();
         this.setState({
@@ -42,7 +54,7 @@ export class News extends Component {
 
     btnNext = async () => {
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             let data = await fetch(url);
             let newData = await data.json(this.state.articles);
             this.setState({
