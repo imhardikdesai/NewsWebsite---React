3bee11606fb4c64498ce2d9a2161752dd2845ba1
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-25 17:26:29 +0530

    refactoring news components

--- src/components/News.js
+++ src/components/News.js
@@ -15,20 +15,24 @@ export class News extends Component {
         category: PropTypes.string,
         pageSize: PropTypes.number
     }
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} - NewsMonkey`;
     }
 
     async componentDidMount() {
-        this.setState({
-            loading: true
-        })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=1&pageSize=${this.props.pageSize}`;
+        this.updateNews();
+    }
+
+    async updateNews() {
+        this.setState({ loading: true })
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page}& pageSize=${this.props.pageSize} `;
         let data = await fetch(url);
         let newData = await data.json();
         this.setState({
@@ -37,42 +41,43 @@ export class News extends Component {
             loading: false
         })
     }
-
     btnPrev = async () => {
-        this.setState({
-            loading: true
-        })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        let newData = await data.json();
-        this.setState({
-            articles: newData.articles,
-            page: this.state.page - 1,
-            loading: false
-        })
+        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        // let data = await fetch(url);
+        // let newData = await data.json();
+        // this.setState({ loading: true })
+        // this.setState({
+        //     articles: newData.articles,
+        //     page: this.state.page - 1,
+        //     loading: false
+        // })
+        this.setState({ page: this.state.page - 1 })
+        this.updateNews();
     }
 
     btnNext = async () => {
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            let data = await fetch(url);
-            let newData = await data.json(this.state.articles);
-            this.setState({
-                articles: newData.articles,
-                totalResults: newData.totalResults,
-                page: this.state.page + 1
-            })
-        }
+        // if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+        //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5ee41684e9124d3382b0e5416bb89437&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        //     let data = await fetch(url);
+        //     let newData = await data.json(this.state.articles);
+        //     this.setState({
+        //         articles: newData.articles,
+        //         totalResults: newData.totalResults,
+        //         page: this.state.page + 1
+        //     })
+        // }
+        this.setState({ page: this.state.page + 1 })
+        this.updateNews();
     }
     render() {
         return (
             <div className='container my-3'>
-                <h1 className='text-center'>Top Headlines</h1>
+                <h1 className='text-center'>Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}  Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : "Empty Title"} imageUrl={element.urlToImage ? element.urlToImage : "https://asianatimes.com/wp-content/uploads/2022/08/asteroid-6054186_1920_1659586399339_1659712789934_1659712789934.jpg"} description={element.description ? element.description : "Empty Description"} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+                            <NewsItem title={element.title ? element.title : "Empty Title"} imageUrl={element.urlToImage ? element.urlToImage : "https://asianatimes.com/wp-content/uploads/2022/08/asteroid-6054186_1920_1659586399339_1659712789934_1659712789934.jpg"} description={element.description ? element.description : "Empty Description"} url={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card">
-                    <span style={{zIndex:"1", left:"90%"}} className="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
-                        {source}aahaha
+                    <span style={{ zIndex: "1", left: "90%" }} className="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
+                        {source}
                     </span>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    {/* <img src={imageUrl} className="card-img-top" alt="..." /> */}
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
